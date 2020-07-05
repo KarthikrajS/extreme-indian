9163cfe03d3eda05735227745a7a6c85f8ff8430
Karthikraj S <karthikrajs@Techyugas-MacBook-Pro.local>
2020-07-06 01:25:48 +0530

    soft Shadows

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React,{useRef} from 'react';
 import "./App.scss";
 
 import {Canvas,useFrame} from 'react-three-fiber';
-//import {Box,Circle} from 'drei';
+import {softShadows} from 'drei';
 
-
+softShadows();
 
 const SpinningMesh =({position,args,color})=>{
     const mesh=useRef(null);
     useFrame(()=>(mesh.current.rotation.x=mesh.current.rotation.y += 0.01))
     return(
-        <mesh ref={mesh} position={position}>
+        <mesh ref={mesh} position={position} castShadow>
             <boxBufferGeometry attach='geometry' args={args} />
             <meshStandardMaterial attach='material' color={color}/>
         </mesh>
@@ -21,7 +21,7 @@ const Circle =()=>{
     const mesh=useRef(null);
     useFrame(()=>(mesh.current.rotation.x=mesh.current.rotation.y += 0.01))
     return(
-        <mesh ref={mesh}>
+        <mesh ref={mesh} castShadow>
             <circleBufferGeometry attach='geometry' args={[1,1,1]} />
             <meshStandardMaterial attach='material' color=''/>
         </mesh>
@@ -29,23 +29,25 @@ const Circle =()=>{
 }
 class App extends React.Component{
 
+
     render(){
 
         return (
             <>
                 <Canvas shadowMap colorManagement camera={{position:[-5,2,10],fov:60}}>
                     <ambientLight intensity={0.3} />
+                    <directionalLight castShadow poition={[0,10,0]} intensity={1}  shadow-mapSize-width={1024} shadow-mapSize-height={1024}
+                                      shadow-camera-far={50} shadow-camera-left={-10} shadow-camera-right={10} shadow-camera-top={10} shadow-camera-bottom={-10} />
                     <pointLight poition={[-10,0,-20]}  intensity={0.5} />
                     <pointLight poition={[0,-10,0]} intensity={1.5} />
-
                     <group>
-                        <mesh rotation={[-Math.PI /2 , 0 , 0]} poition={[0,-3,0]}>
+                        <mesh receiveShadow rotation={[-Math.PI /2 , 0 , 0]} position={[0,-3,0]}>
                             <planeBufferGeometry attach='geometry' args={[100,100]}/>
-                            <meshStandardMaterial attach='material' color='yellow'/>
+                            <shadowMaterial attach='material' opacity={0.3} />
+
                         </mesh>
                     </group>
-                    <directionalLight poition={[0,10,0]} intensity={1}  shadow-mapSize-shadowMapWidth={1024} shadow-mapSize-shadowMapHeight={1024}
-                        shadow-camera-far={50} shadow-camera-left={-10} shadow-camera-right={10} shadow-camera-top={10} shadow-camera-bottom={-10} />
+
                     <SpinningMesh position={[0,1,0]} args={[3,2,1]} color='lightblue'/>
                     <SpinningMesh position={[-2,1,-5]} color='pink'/>
                     <SpinningMesh position={[5,1,-2]} color='pink'/>
